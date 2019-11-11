bb9410270cc3ea5de8cae61223271eeae3bd0568
Rubezhnole <kot32289>
2019-11-11 23:47:57 +0200

    Init project.

---
+++ src/main/java/com/kotl/tacoclous/TacoClousApplication.java
@@ -0,0 +1,13 @@
+package com.kotl.tacoclous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoClousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoClousApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kotl/tacoclous/TacoClousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotl.tacoclous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoClousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
